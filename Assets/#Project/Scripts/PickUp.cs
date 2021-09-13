b742038be47ae6fd24999478cad0d54e446ab924
Belorina <ozcan_berrin@hotmail.com>
2021-09-13 12:18:25 +0200

    floppy disk destroy

--- Assets/#Project/Scripts/PickUp.cs
+++ Assets/#Project/Scripts/PickUp.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public string itemToPickUp;
+
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.gameObject.CompareTag(itemToPickUp))
+        {
+            Destroy(other.gameObject);
+        }
+
     }
 }
