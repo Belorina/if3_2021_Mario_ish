2e4300e24ea433da30bc2ed7525644122279a7cd
Belorina <ozcan_berrin@hotmail.com>
2021-09-13 14:59:22 +0200

    opendoor with scenechanger / load + new moving platformes

--- Assets/#Project/Scripts/Door.cs
+++ Assets/#Project/Scripts/Door.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour
 {
-    public string destination;
+    public bool isOpen = true;
 
-    void OnTriggerEnter2D(Collider2D other)
+    public Sprite doorOpen;
+    public Sprite doorClosed;
+    public UnityEvent whenEnter;
+
+    //public string destination;
+
+    private void Start()
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (isOpen)
         {
-            SceneManager.LoadScene(destination);
+            GetComponent<SpriteRenderer>().sprite = doorOpen;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = doorClosed;
         }
     }
 
+
+    private void onTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && isOpen)
+        {
+            whenEnter?.Invoke();
+        }
+    }
+
+    public void Unlock()
+    {
+        isOpen = true;
+        GetComponent<SpriteRenderer>().sprite = doorOpen;
+    }
+
+    // public void SceneChanger()
+    // {
+    //     SceneManager.LoadScene("SecondScene");
+
+    // }
 }
